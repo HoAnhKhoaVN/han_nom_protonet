8b8244f1e234d5db939e73742c08bfb739f99152
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-08-16 23:59:16 +0700

    Change root folder

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
-python3 train.py -root fsl_train_test_val \
+python3 train.py -root /content/dataset_han_nom \
                  -exp /content/drive/MyDrive/Master/NLP/ProtoNet_run_on_colab \
                  -nep 60000 \
                  -lr 1e-3 \
